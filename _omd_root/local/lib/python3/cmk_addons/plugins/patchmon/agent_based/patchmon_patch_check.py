4b5b968730ba9b82dc88fd3f852d4618af4582cd
Mattias Schlenker <mattias.schlenker@checkmk.com>
2026-02-17 07:01:17 +0100

    properly format service output

--- _omd_root/local/lib/python3/cmk_addons/plugins/patchmon/agent_based/patchmon_patch_check.py
+++ _omd_root/local/lib/python3/cmk_addons/plugins/patchmon/agent_based/patchmon_patch_check.py
@@ -19,13 +19,26 @@ def discover_patchmon_reboot(section):
 def check_patchmon_patches(params, section):
     sec = section['security_updates']
     tot = section['outdated_packages']
+    messages = [
+        "No updates are missing. View on PatchMon: {url}.",
+        "{tot} updates missing. View on PatchMon: {url}.",
+        "{sec} important security updates missing ({tot}  total)! View on PatchMon: {url}."
+    ]
     if sec > 0:
-        yield Result(state=State(params['statesecurity']), summary=str(sec)  + " important security updates missing (" + str(tot) + " total)!")
+        yield Result(
+            state=State(params['statesecurity']), 
+            summary=messages[2].format(url=section['url'], tot=tot, sec=sec)
+        )
     elif tot > 0:
-        yield Result(state=State(params['stateregular']), summary=str(tot) + " updates are missing.")
+        yield Result(
+            state=State(params['stateregular']),
+            summary=messages[1].format(url=section['url'], tot=tot)
+        )
     else:
-        yield Result(state=State.OK, summary="No updates are missing.")
-    yield Result(state=State.OK, summary="View on PatchMon: " + section['url'])
+        yield Result(
+            state=State.OK,
+            summary=messages[0].format(url=section['url'])
+        )
     yield Metric(
         name = "packages_outdated",
         value = tot,
@@ -37,12 +50,21 @@ def check_patchmon_patches(params, section):
     
 def check_patchmon_reboot(params, section):
     if not "needs_reboot" in section:
-        yield Result(state=State.UNKNOWN, summary="Reboot information missing. Did someone change the special agent configuration?")
+        yield Result(
+            state=State.UNKNOWN,
+            summary="Reboot information missing. Did someone change the special agent configuration?"
+        )
         return
     if section['needs_reboot'] > 0:
-        yield Result(state=State(params['statereboot']), summary="Reboot required, reason given: " + section['reboot_reason'])
+        yield Result(
+            state=State(params['statereboot']),
+            summary="Reboot required, reason given: " + section['reboot_reason']
+        )
     else:
-        yield Result(state=State.OK, summary="No reboot needed.")
+        yield Result(
+            state=State.OK,
+            summary="No reboot needed."
+        )
 
 agent_section_patchmon_patches = AgentSection(
     name = "patchmon_patches",
@@ -54,7 +76,11 @@ check_plugin_patchmon_patches = CheckPlugin(
     service_name = "PatchMon patches",
     discovery_function = discover_patchmon_patches,
     check_function = check_patchmon_patches,
-    check_default_parameters = { "stateregular": int(State.WARN), "statesecurity": int(State.CRIT), "statereboot": int(State.CRIT) },
+    check_default_parameters = { 
+        "stateregular": int(State.WARN), 
+        "statesecurity": int(State.CRIT), 
+        "statereboot": int(State.CRIT) 
+    },
     check_ruleset_name = "patchmon_patches",
 )
 
@@ -64,6 +90,10 @@ check_plugin_patchmon_reboot = CheckPlugin(
     service_name = "PatchMon reboot required",
     discovery_function = discover_patchmon_reboot,
     check_function = check_patchmon_reboot,
-    check_default_parameters = { "stateregular": int(State.WARN), "statesecurity": int(State.CRIT), "statereboot": int(State.CRIT) },
+    check_default_parameters = { 
+        "stateregular": int(State.WARN), 
+        "statesecurity": int(State.CRIT), 
+        "statereboot": int(State.CRIT) 
+    },
     check_ruleset_name = "patchmon_patches",
 )
