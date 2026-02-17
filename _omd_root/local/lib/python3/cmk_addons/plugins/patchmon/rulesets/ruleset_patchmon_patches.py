ed1555d38d84e31073b58e4a059e7a6f6fc7a85e
Mattias Schlenker <mattias.schlenker@checkmk.com>
2026-02-17 06:46:09 +0100

    properly format help

--- _omd_root/local/lib/python3/cmk_addons/plugins/patchmon/rulesets/ruleset_patchmon_patches.py
+++ _omd_root/local/lib/python3/cmk_addons/plugins/patchmon/rulesets/ruleset_patchmon_patches.py
@@ -16,12 +16,18 @@ from cmk.rulesets.v1.rule_specs import (
 
 def _parameter_form():
     return Dictionary(
-        help_text = Help("Specify the default states of the two services \"PatchMon patches\" and \"PatchMon reboot\"."),
+        help_text = Help(
+            "Specify the default states of the two services \"PatchMon patches\" and \"PatchMon "
+            " reboot\"."
+        ),
         elements = {
             "stateregular": DictElement(
                 parameter_form = ServiceState(
                     title = Title("Service state when regular updates are missing"),
-                    help_text = Help("Apply the state specified here to the patch service if updates are missing, but none of these updates is classified as security update."),
+                    help_text = Help(
+                        "Apply the state specified here to the patch service if updates are "
+                        "missing, but none of these updates is classified as security update."
+                    ),
                     prefill = DefaultValue(ServiceState.WARN),
                 ),
                 required = True,
@@ -29,7 +35,11 @@ def _parameter_form():
             "statesecurity": DictElement(
                 parameter_form = ServiceState(
                     title = Title("Service state when security updates are missing"),
-                    help_text = Help("Apply the state specified here to the patch service if updates are missing and at least one of these update is classified as security update."),
+                    help_text = Help(
+                        "Apply the state specified here to the patch service if updates are "
+                        "missing and at least one of these update is classified as security "
+                        "update."
+                    ),
                     prefill = DefaultValue(ServiceState.CRIT),
                 ),
                 required = True,
@@ -37,7 +47,10 @@ def _parameter_form():
             "statereboot": DictElement(
                 parameter_form = ServiceState(
                     title = Title("Service state when reboot need is detected"),
-                    help_text = Help("Apply the state specified here to the reboot service if the flag \"reboot needed\" is detected by PatchMon."),
+                    help_text = Help(
+                        "Apply the state specified here to the reboot service if the flag \"reboot "
+                        "needed\" is detected by PatchMon."
+                    ),
                     prefill = DefaultValue(ServiceState.CRIT),
                 ),
                 required = True,
